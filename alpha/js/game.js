c48a0759cded799889412e72c6a9d859b0e0e7d6
P3RK STUDIOS <parth.kaushal.007@gmail.com>
2021-06-20 10:50:30 +0530

    responsive canvas and random position

--- alpha/js/game.js
+++ alpha/js/game.js
@@ -1,10 +1,12 @@
 let { init, Sprite, GameLoop } = kontra
 
 let { canvas } = init();
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
 
 let sprite = Sprite({
-  x: 100,        // starting x,y position of the sprite
-  y: 80,
+  x: getRandom(window.innerWidth),        // starting x,y position of the sprite
+  y: getRandom(window.innerHeight),
   color: 'gold',  // fill color of the sprite rectangle
   width: 20,     // width and height of the sprite rectangle
   height: 40,
@@ -15,16 +17,20 @@ let colorList = ["violet", "indigo", "blue", "green", "yellow", "orange", "red"]
 
 let loop = GameLoop({  // create the main game loop
   update: function() { // update the game state
-    // setTimeout( () => {
-    // } , 1000)
-    
+
+    if (counter() == 0){
+      sprite.x = getRandom(window.innerWidth)
+      sprite.y = getRandom(window.innerHeight)
+    }
+
     sprite.update();
 
     // wrap the sprites position when it reaches
     // the edge of the screen
     if (sprite.x > canvas.width) {
       sprite.x = -sprite.width;
-      sprite.color = colorList[Math.floor(Math.random() * 7)];
+      sprite.color = colorList[getRandom(7)];
+      
     }
   },
   render: function() { // render the game state
@@ -33,3 +39,27 @@ let loop = GameLoop({  // create the main game loop
 });
 
 loop.start();    // start the game
+
+
+function changeCanvasSize(){
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+}
+
+
+window.onresize = changeCanvasSize
+
+function getRandom(maxValue){
+  return Math.floor(Math.random() * maxValue)
+}
+
+var c = 0
+
+function counter(){
+  c++
+  if (c > 250) {
+    c = 0
+  }
+  return c
+}
+
